a128efd2744f3180822dc3bc9a0522827cf50390
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-12 20:22:38 -0300

    Continuando projeito, criando função de renderizar

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const descricao = document.getElementById("descricao");
-const imagens = document.getElementById("imagens");
 const URL_API = "https://pokeapi.co/api/v2/pokedex/";
 
 
-console.log(dados);
-
-const buscarPokemon = pokemon => {
-    const URL = fetch("https://pokeapi.co/api/v2/pokedex/");
-    console.log(URL);
-    const dados = URL.json()
-    console.log(dados);
-    
+const buscarPokemon = async (pokemon) => {
+    const URL = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+    const dados = await URL.json()
+    console.log((dados));
 }
+
+const renderizar = () => {
+    const descricao = document.getElementById("descricao");
+    const imagens = document.getElementById("imagens");
+    const inputPokedex = document.getElementById("inputPokemon")
+    const numeroPokedex = inputPokedex.value
+
+    imagens.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png"
+    descricao.textContent = "dlkajwbdajwhbd"
+}
+
+renderizar()
